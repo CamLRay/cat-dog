58294d12f7a21f6891dfd44f061602671b72caa4
Sploob <crayiphone@gmail.com>
2022-03-22 14:07:14 -0700

    add ability to send messages as a cat with responses from a dog
    Co-authored-by: William Rodriguez <william@rodriguez1.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
+  $("#neutral").click(function() {
+    const catResponse = $("<li>Meow.</li>");
+    $("#response").prepend(catResponse);
+    console.log(catResponse);
+    setTimeout(function () {
+      catResponse.before("<li>Woof.</li>");
+    }, 500);
+  });
 
+  $("#excited").click(function() {
+    const catResponse = $("<li>Meow meow!</li>");
+    $("#response").prepend(catResponse);
+    setTimeout(function () {
+      catResponse.before("<li>BARK BARK BARK!</li>");
+    }, 500);
+  });
+
+  $("#angry").click(function() {
+    const catResponse = $("<li>Meow... MEOW MEOW!</li>");
+    $("#response").prepend(catResponse);
+    setTimeout(function () {
+      catResponse.before("<li class='anger'>BARK BARK BARK!</li>");
+    }, 500);
+  });
 });
+
